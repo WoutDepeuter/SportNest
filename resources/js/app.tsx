c948588c8f1064ee730393a0d44cededca982d40
Amelia <77553571+Fesaa@users.noreply.github.com>
2024-12-10 11:02:32 +0100

    Add react stuff

---
+++ resources/js/app.tsx
@@ -0,0 +1,20 @@
+import { createRoot } from 'react-dom/client';
+import { createInertiaApp } from '@inertiajs/react';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+
+createInertiaApp({
+    title: title => "Title",
+    resolve: name =>
+        resolvePageComponent(
+            `./Pages/${name}.tsx`,
+            import.meta.glob('./Pages/**/*.tsx')
+        ),
+    setup({ el, App, props }) {
+        const root = createRoot(el);
+
+        root.render(<App {...props} />);
+    },
+    progress: {
+        color: '#F87415'
+    }
+});
