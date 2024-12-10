976b3b56c1a79eea2d38b264de60613072fbae6a
Fesa <77553571+Fesaa@users.noreply.github.com>
2024-12-10 15:04:03 +0100

    Seeder script for sports
    This is only from one csv,
    https://datastore.brussels/web/data/dataset/ef01e4cb-57ff-4b5e-9def-bdb31f199054,
    others may be added later.

--- app/Models/SportClub.php
+++ app/Models/SportClub.php
@@ -2,17 +2,24 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class SportClub extends Model
 {
-    /** @use HasFactory<\Database\Factories\SportClubFactory> */
-    use HasFactory;
 
     public string $name;
+
+    public string $url;
+
+    /**
+     * @return Address
+     */
+    public function Address(): HasOne
+    {
+        return $this->hasOne(Address::class);
+    }
 
     /**
      * @return Sport[]
