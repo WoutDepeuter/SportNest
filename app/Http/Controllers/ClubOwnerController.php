c58ed96afc186116d1d7892315a596deacdef1de
dominium2 <danial.mubeasher@student.ehb.be>
2024-12-10 14:31:15 +0100

    new club owner pages an routing

--- app/Http/Controllers/ClubOwnerController.php
+++ app/Http/Controllers/ClubOwnerController.php
@@ -1,10 +1,23 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Inertia\Inertia;
 use Illuminate\Http\Request;
 
 class ClubOwnerController extends Controller
 {
-    //
+    public function Home()
+    {
+        return Inertia::render('ClubOwner/ClubOwnerPage');
+    }
+
+    public function ClubAdd()
+    {
+        return Inertia::render('ClubOwner/AddClubPage');
+    }
+
+    public function ClubEdit()
+    {
+        return Inertia::render('ClubOwner/EditClubPage');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,15 @@ Route::get('/club/{id}', [UserController::class, 'club']);
 
 #FAQ page route
 Route::get('/faq', [UserController::class, 'faq']);
+
+#--------------------------------------------
+use App\Http\Controllers\ClubOwnerController;
+
+#Club Owner Home route
+Route::get('/clubowner', [ClubOwnerController::class, 'Home']);
+
+#Club Owner Add Club route
+Route::get('/clubowner/club/add', [ClubOwnerController::class, 'ClubAdd']);
+
+#Club Owner Edit Club route
+Route::get('/clubowner/club/edit', [ClubOwnerController::class, 'ClubEdit']);
