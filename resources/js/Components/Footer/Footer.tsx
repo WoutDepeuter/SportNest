dd5b70bf7df39b1e4d5ae7bae299a485a40e9a02
tristanvong <146710405+tristanvong@users.noreply.github.com>
2024-12-10 15:42:27 +0100

    Create Footer.tsx

---
+++ resources/js/Components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+    return (
+        <footer className="dark:bg-gray-800 p-4 fixed bottom-0 left-0 w-full">
+            <div className="flex items-center">
+                <div className="ml-auto bg-gray-100 rounded-full px-4 py-2 transition-colors duration-200 hover:bg-red-500">
+                    <a href="#" >FaQ</a>
+                </div>
+            </div>
+        </footer>
+    );
+}
