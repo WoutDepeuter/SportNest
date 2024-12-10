20134808862487e314e7ac19a2979cd8394c8d5b
tristanvong <146710405+tristanvong@users.noreply.github.com>
2024-12-10 15:53:14 +0100

    Made footer

--- resources/js/Components/Footer/Footer.tsx
+++ resources/js/Components/Footer/Footer.tsx
@@ -2,9 +2,13 @@ export default function Footer() {
     return (
         <footer className="dark:bg-gray-800 p-4 fixed bottom-0 left-0 w-full">
             <div className="flex items-center">
-                <div className="ml-auto bg-gray-100 rounded-full px-4 py-2 transition-colors duration-200 hover:bg-red-500">
-                    <a href="#" >FaQ</a>
+                <div className="mb-4 md:mb-0">
+                    <a href="#" className="text-lg font-bold text-white">SportNest</a>
+                    <p className="text-sm text-white">© 2024 Programming Project</p>
                 </div>
+                <a href="#" className="ml-auto bg-gray-100 rounded-full px-4 py-2 transition-colors duration-200 hover:bg-red-500">
+                    <div>FaQ</div>
+                </a>
             </div>
         </footer>
     );
