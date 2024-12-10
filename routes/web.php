de37021aa6dcbe41a5650c783659151dd6caf40f
dominium2 <danial.mubeasher@student.ehb.be>
2024-12-10 13:49:10 +0100

    adding new pages and routing

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,22 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
+#homepage route
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'user' => "A username",
-        "test" => 1
-    ]);
+    return Inertia::render('Homepage');
+});
+
+#Search Result route
+Route::get('/Search-Result', function () {
+    return Inertia::render('SearchResult');
+});
+
+#club page route
+Route::get('/Club-Page', function () {
+    return Inertia::render('ClubPage');
+});
+
+#FAQ page route
+Route::get('/FAQ', function () {
+    return Inertia::render('FAQPage');
 });
