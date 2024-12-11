5edf783d0475ad207b9e72cd276c10a3b70048ce
HamzaKh46 <hamza20khayi@gmail.com>
2024-12-11 13:56:39 +0100

    Review pop up

--- resources/js/Pages/Homepage.tsx
+++ resources/js/Pages/Homepage.tsx
@@ -1,14 +1,18 @@
 
+import ParentReview from '@/Components/Popups/ParentReview';
 import { Head } from '@inertiajs/react'
 import React from 'react';
 
 export default function Homepage() {
     return (
-        <div>
+        <><div>
             <Head title="Homepage" />
             <h1>Welcome</h1>
             <p>This is the homepage without variables.</p>
-        </div>
+        </div><div>
+                <h1>Welcome to the Review App</h1>
+                <ParentReview /> 
+            </div></>
     );
 }
 
