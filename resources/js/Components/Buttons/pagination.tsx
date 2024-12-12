1dd7408f36306a1b618a5a5dfd66764f8e16281b
Fesa <77553571+Fesaa@users.noreply.github.com>
2024-12-12 15:18:50 +0100

    Add back only back/next pagination

--- resources/js/Components/Buttons/pagination.tsx
+++ resources/js/Components/Buttons/pagination.tsx
@@ -72,3 +72,37 @@ const Pagination: React.FC<PaginationProps> = ({
 };
 
 export default Pagination;
+
+type SoloPaginationProps = {
+    back: () => void;
+    next: () => void;
+    hasNext: () => boolean;
+    hasPrevious: () => boolean;
+    classes?: string
+}
+
+export function SoloPagination(props: SoloPaginationProps) {
+    return (
+        <div className={`flex ` + (props.classes ?? "")}>
+            {props.hasPrevious() && <button
+                onClick={() => {
+                    props.back();
+                }}
+                className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l"
+            >
+                Prev
+            </button>}
+
+            {props.hasNext() && <button
+                onClick={() => {
+                    props.next();
+                }}
+                className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-r"
+            >
+                Next
+            </button>}
+
+
+        </div>
+    );
+}
