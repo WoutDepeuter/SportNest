41d857f0b6b9599f17d667f67e175205118b2cbf
depeu <depeuterwout@gmail.com>
2024-12-12 11:19:09 +0100

    made buttons uniform

--- resources/js/Components/ClubPage/LeaveReviewButton.tsx
+++ resources/js/Components/ClubPage/LeaveReviewButton.tsx
@@ -1,5 +1,5 @@
 export default function LeaveReviewButton() {
     return (
-        <a href="/review" className={"bg-gray-200 text-2xl font-bold p-4 border-gray-200 border"}> LEAVE AN REVIEW </a>
+        <a href="/review" className={"inline-flex items-center bg-gray-200 text-2xl font-bold px-6 py-3 border border-gray-400 rounded-lg m-4 shadow-md hover:bg-gray-300"}> LEAVE AN REVIEW </a>
     )
 }
