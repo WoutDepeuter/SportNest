2d3ef746af35c4f20c3be04d352fa6f0ddb1044b
depeu <depeuterwout@gmail.com>
2024-12-12 11:14:28 +0100

    fix typo

--- resources/js/Components/ClubPage/LeaveReviewButton.tsx
+++ resources/js/Components/ClubPage/LeaveReviewButton.tsx
@@ -1,5 +1,5 @@
 export default function LeaveReviewButton() {
     return (
-        <a href="/review" className={"bg-gray-200 text-2xl font-bold p-4 border-gray-200 border"}> LEAVE A REVIEW </a>
+        <a href="/review" className={"bg-gray-200 text-2xl font-bold p-4 border-gray-200 border"}> LEAVE AN REVIEW </a>
     )
 }
