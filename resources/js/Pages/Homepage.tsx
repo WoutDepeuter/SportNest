00bf795bc7271a597977aa259232e6756c787e55
dominium2 <danial.mubeasher@student.ehb.be>
2024-12-18 10:50:10 +0100

    changes homepage banner and size, also changed map

--- resources/js/Pages/Homepage.tsx
+++ resources/js/Pages/Homepage.tsx
@@ -6,18 +6,16 @@ import MainLayout from "@/Layouts/MainLayout";
 function Homepage(): JSX.Element {
     return (
         <div className="flex flex-col w-full flex-grow">
-            <div className="relative flex items-center justify-center h-4/5 p-2">
-                {/* TODO: Find better image, make smaller, idk how */}
-                <img src="/images/Sport-banner.jpg" alt="Banner image"
-                     className="rounded-2xl shadow w-full h-full object-cover"/>
-                <a href="/quiz/"
-                    className="absolute bg-red-500 text-white font-bold py-2 px-4 rounded hover:bg-red-600 transform -rotate-30">
-                    Take Quiz
-                </a>
+            <div className="relative flex items-center justify-center w-screen h-[31.25rem] p-2 bg-cover bg-center mx-auto" style={{ backgroundImage: "url('/images/homepage/braden-collum-ttbCwN_mWic-unsplash.jpg')", backgroundSize: 'cover', backgroundPosition: 'center' }}>
+            <a href="/quiz/"
+                className="absolute bottom-4 bg-red-500 text-white font-bold py-4 px-8 rounded hover:bg-red-600">
+                Take Quiz
+            </a>
             </div>
 
-
-            {/* TODO: Homepage */}
+            <div className="flex flex-grow justify-center items-center m-5 p-5">
+                <Map />
+            </div>
         </div>
     );
 }
