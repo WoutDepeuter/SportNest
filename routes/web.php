fb5ec2662e761adf334a2a48ae12f7ea960d4ec2
depeu <depeuterwout@gmail.com>
2024-12-18 12:04:08 +0100

    test page voor testing

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\UserController;
 use Inertia\Inertia;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 
+
 #homepage route
 Route::get('/', [UserController::class, 'index']);
 
@@ -18,6 +19,10 @@ Route::get('/club/{id}', [UserController::class, 'club']);
 
 #FAQ page route
 Route::get('/faq', [UserController::class, 'faq']);
+
+Route::get('/test', function () {
+    return Inertia::render('TestPage');
+});
 
 #--------------------------------------------
 use App\Http\Controllers\ClubOwnerController;
@@ -40,7 +45,7 @@ Route::get('/clubowner/club/add', [ClubOwnerController::class, 'ClubAdd'])->midd
 Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
     ->middleware('auth')
     ->name('logout');
-    
+
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
