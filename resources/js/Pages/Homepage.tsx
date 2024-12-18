475a2cbf68e83d29764d370396f8b37355bfbc49
dominium2 <danial.mubeasher@student.ehb.be>
2024-12-18 10:57:24 +0100

    changed banner image name

--- resources/js/Pages/Homepage.tsx
+++ resources/js/Pages/Homepage.tsx
@@ -6,7 +6,7 @@ import MainLayout from "@/Layouts/MainLayout";
 function Homepage(): JSX.Element {
     return (
         <div className="flex flex-col w-full flex-grow">
-            <div className="relative flex items-center justify-center w-screen h-[31.25rem] p-2 bg-cover bg-center mx-auto" style={{ backgroundImage: "url('/images/homepage/braden-collum-ttbCwN_mWic-unsplash.jpg')", backgroundSize: 'cover', backgroundPosition: 'center' }}>
+            <div className="relative flex items-center justify-center w-screen h-[31.25rem] p-2 bg-cover bg-center mx-auto" style={{ backgroundImage: "url('/images/homepage/Sport-banner.jpg')", backgroundSize: 'cover', backgroundPosition: 'center' }}>
             <a href="/quiz/"
                 className="absolute bottom-4 bg-red-500 text-white font-bold py-4 px-8 rounded hover:bg-red-600">
                 Take Quiz
