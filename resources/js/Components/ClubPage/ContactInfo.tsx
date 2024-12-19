f3f73506319aa5e33034219b0c0a28e9e0c754c2
depeu <depeuterwout@gmail.com>
2024-12-19 11:46:30 +0100

    clubpage 4

--- resources/js/Components/ClubPage/ContactInfo.tsx
+++ resources/js/Components/ClubPage/ContactInfo.tsx
@@ -1,9 +1,6 @@
-import PhoneSVG from "@/Components/ClubPage/SVG/PhoneSVG";
-import MailSVG from "@/Components/ClubPage/SVG/MailSVG";
-import BrowserSVG from "@/Components/ClubPage/SVG/BrowserSVG";
-import LeaveReviewButton from "@/Components/ClubPage/LeaveReviewButton";
+import {EnvelopeIcon, GlobeAltIcon, PhoneIcon} from "@heroicons/react/24/outline";
 import ReportButton from "@/Components/ClubPage/ReportButton";
-import Map from "@/Components/Map";
+import {LeaveReviewButton} from "@/Components/ClubPage/Genericbutton";
 
 type ContactInfoProps = {
     website: string;
@@ -16,31 +13,28 @@ export default function ContactInfo({ website, email, phone }: ContactInfoProps)
         <div className="contact-info border-gray-200 border bg-gray-200 max-w-xs p-6 rounded-lg shadow-lg">
             <h1 className="text-xl font-semibold mb-6">Contact</h1>
 
-            <map />
-
             <div className="flex flex-col space-y-6">
                 <div className="flex items-center">
-                    <BrowserSVG />
+                    <GlobeAltIcon/>
                     <p className="mt-0 ml-2">Website: {website}</p>
                 </div>
 
                 <div className="flex items-center">
-                    <MailSVG />
+                    <EnvelopeIcon/>
                     <p className="mt-0 ml-2">Email: {email}</p>
                 </div>
 
                 <div className="flex items-center">
-                    <PhoneSVG />
+                    <PhoneIcon/>
                     <p className="mt-0 ml-2">Phone: {phone}</p>
                 </div>
-            </div>
-
-            <div className="flex flex-col gap-4 mt-6 items-start">
-                <div className="w-auto">
-                    <LeaveReviewButton />
-                </div>
-                <div className="w-auto">
-                    <ReportButton />
+                <div className="flex flex-col gap-4 mt-6 items-start">
+                    <div className="w-auto">
+                        <LeaveReviewButton/>
+                    </div>
+                    <div className="w-auto">
+                        <ReportButton/>
+                    </div>
                 </div>
             </div>
         </div>
