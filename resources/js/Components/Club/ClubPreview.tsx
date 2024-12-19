83698259a14dc4262ffd9f43ef1a540d8bd2eab0
Fesa <77553571+Fesaa@users.noreply.github.com>
2024-12-19 10:56:10 +0100

    Fix layout of club searc

--- resources/js/Components/Club/ClubPreview.tsx
+++ resources/js/Components/Club/ClubPreview.tsx
@@ -8,11 +8,11 @@ type ClubPreviewProps = {
 export default function ClubPreview(props: ClubPreviewProps) {
     const club = props.club;
 
-    return  <div className="flex items-center p-4 border border-gray-200 rounded-lg shadow-lg bg-white w-full">
+    return  <div className="flex items-center p-4 border border-gray-200 rounded-lg shadow-lg bg-white w-96 h-32">
         <div className="flex-1">
-            <h2 className="text-xl font-bold">{Titleize(club.name)}</h2>
+            <a href={"/club/" + club.id} className="text-xl font-bold hover:underline">{Titleize(club.name)}</a>
             <p className="text-gray-600">
-                Adress placeholder
+                Address placeholder
             </p>
             <a
                 href={club.website_url}
